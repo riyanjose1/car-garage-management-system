e6e1ba69254ec5ff61bf10e1ec1305a670ecb5ac
riyan jose <riyanjose1@gmail.com>
2026-03-30 21:06:34 +0530

    Brand app as AutoServe

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>{{ config('app.name', 'MyProject') }}</title>
+    <title>AutoServe</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('images/autoserve-mark.svg') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&display=swap" rel="stylesheet">
